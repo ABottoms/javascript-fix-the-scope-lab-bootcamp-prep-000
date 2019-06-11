bb8c14b5fbd694b6bb1ec7d68131ae628a1a1765
salubrious-chain-5511 <anisa.bottoms@gmail.com>
2019-06-11 18:07:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-var animal = 'dog'
-
+var myAnimal = ('dog'
+)
 function myAnimal() {
-  return animal
+  return (string) ('dog')
 }
-
+var yourAnimal = ('cat')
 function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
-  return animal
+  return (string) ('cat')
 }
-
 function add2(n) {
+console.log(parseInt('2' += 'n'));
   return n + two
 
   // Feel free to move things around!
@@ -26,4 +26,8 @@ var funkyFunction = function() {
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction
+var funkyFunction = "theFunk"() {
+console.log (funkyFunction) 
+console.log ('FUNK!') 
+ return 'theFunk'
+
